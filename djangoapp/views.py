29ff15ba2f8c03fffe5c272d57eceb4d4c96b922
NichZilli <nich.zilli@hotmail.com>
2024-05-23 17:08:00 -0300

    Initial Files

---
+++ djangoapp/views.py
@@ -0,0 +1,23 @@
+from rest_framework import APIView
+from rest_framework.response import Response
+
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+class CursoAPIView(APIView):
+    """
+    Test
+    """
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+class AvaliacaoAPIView(APIView):
+    """
+    Test 2
+    """
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
